c32ba3489727363292fcdd824adf8c4af7ceb3a6
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-12 11:10:57 +0300

    Tut3 class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -1,48 +1,11 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
+from rest_framework import generics
 from .models import Snippet
 from .serializers import SnippetSerializer
 
-@api_view(['GET', 'POST'])
-def snippet_list(req, format=None):
-  """
-  List all code snippets, or create a new snippet
-  """
-  if req.method == 'GET':
-    snippets = Snippet.objects.all()
-    serializer = SnippetSerializer(snippets, many=True)
-    return Response(serializer.data)
-  elif req.method == 'POST':
-    serializer = SnippetSerializer(data=req.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
+class SnippetList(generics.ListCreateAPIView):
+  queryset = Snippet.objects.all()
+  serializer_class = SnippetSerializer
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(req, id=None, format=None):
-  """
-  Retrieve, update, or delete a code snippet
-  """
-  try:
-    snippet = Snippet.objects.get(pk=id)
-  except Snippet.DoesNotExist:
-    return Response(status=status.HTTP_404_NOT_FOUND)
-
-  if req.method == 'GET':
-    serializer = SnippetSerializer(snippet)
-    return Response(serializer.data)
-
-  elif req.method == 'PUT':
-    serializer = SnippetSerializer(snippet, data=req.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-  elif req.method == 'DELETE':
-    snippet.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Snippet.objects.all()
+  serializer_class = SnippetSerializer
